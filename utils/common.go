7f1da21f3b0d120e56e12cd9cf014eb65c6437ec
hopwesley <ws@sinoc.io>
2021-02-28 17:53:16 +0800

    prepare to setup boot node

--- utils/common.go
+++ utils/common.go
@@ -2,14 +2,42 @@ package utils
 
 import (
 	"fmt"
+	"github.com/herumi/bls-eth-go-binary/bls"
 	"github.com/rs/zerolog"
+	"net"
 	"os"
 )
+
+func init() {
+	_ = bls.Init(bls.BLS12_381)
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateNets = append(privateNets, block)
+	}
+}
 
 func FileExists(fileName string) bool {
 	fileInfo, err := os.Lstat(fileName)
 	if fileInfo != nil || (err != nil && !os.IsNotExist(err)) {
 		return true
+	}
+	return false
+}
+
+var privateNets []*net.IPNet
+
+func IsPrivateIP(ip net.IP) bool {
+	for _, block := range privateNets {
+		if block.Contains(ip) {
+			return true
+		}
 	}
 	return false
 }
